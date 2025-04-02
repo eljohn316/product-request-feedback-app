116c0bfc3620ababd3f74ad44efc584883f38c92
El John <bongaeljohn07@gmail.com>
2025-04-02 14:49:29 +0800

    install and configure neccesary 3rd party middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import router from '@/router';
 
 const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', router);
 
