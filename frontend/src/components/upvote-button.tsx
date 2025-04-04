99e64b2d476b4ef09cfa22bf686cfae0bd36fa19
El John <bongaeljohn07@gmail.com>
2025-04-04 15:13:07 +0800

    fix upvote button active styles

--- frontend/src/components/upvote-button.tsx
+++ frontend/src/components/upvote-button.tsx
@@ -10,15 +10,12 @@ export function UpvoteButton({
   return (
     <button
       className={cn(
-        'group bg-zircon active:bg-royal-blue inline-flex cursor-pointer items-center gap-x-2.5 rounded-[0.625rem] px-4 py-1.5 hover:bg-[#CFD7FF] md:flex-col md:gap-x-0 md:gap-y-2 md:px-[0.6875rem] md:pt-3.5 md:pb-2',
+        'group bg-zircon active:bg-royal-blue text-east-bay [&_svg]:text-royal-blue inline-flex cursor-pointer items-center gap-x-2.5 rounded-[0.625rem] px-4 py-1.5 hover:bg-[#CFD7FF] active:text-white md:flex-col md:gap-x-0 md:gap-y-2 md:px-[0.6875rem] md:pt-3.5 md:pb-2 [&:active_svg]:text-white',
         className
       )}
       {...props}>
-      <ArrowUpIcon
-        aria-hidden="true"
-        className="text-royal-blue group-active:text-white"
-      />
-      <span className="text-east-bay text-[0.8125rem] font-bold -tracking-[0.18px] group-active:text-white">
+      <ArrowUpIcon aria-hidden="true" />
+      <span className="text-[0.8125rem] font-bold -tracking-[0.18px]">
         {children}
       </span>
     </button>
