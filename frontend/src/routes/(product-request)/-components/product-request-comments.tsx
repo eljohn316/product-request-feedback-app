521a89b6c6279606d5ee30325bc4789b5e99aacb
El John <bongaeljohn07@gmail.com>
2025-04-21 16:04:05 +0800

    refactor <ProductRequestComments /> component

--- frontend/src/routes/(product-request)/-components/product-request-comments.tsx
+++ frontend/src/routes/(product-request)/-components/product-request-comments.tsx
@@ -1,8 +1,10 @@
-import { Comment } from '@routes/product-request/-components/comment';
-import { type Comment as TComment } from '@routes/product-request/-lib/types';
+import { Comment as BaseComment } from '@routes/product-request/-components/comment';
+import { type Comment } from '@/lib/types';
 
-export function ProductRequestComments({ comments }: { comments: TComment[] }) {
-  if (comments.length === 0)
+export function ProductRequestComments({ comments }: { comments: Comment[] }) {
+  const totalComments = comments.length;
+
+  if (totalComments === 0)
     return (
       <div className="mt-12 mb-28">
         <h3 className="heading-3 text-east-bay text-center">No comments yet</h3>
@@ -12,16 +14,16 @@ export function ProductRequestComments({ comments }: { comments: TComment[] }) {
   return (
     <div className="rounded-[0.625rem] bg-white p-6 md:px-8">
       <h3 className="heading-3 text-east-bay mb-6 md:mb-7">
-        {comments.length} comments
+        {totalComments} {totalComments === 1 ? 'comment' : 'comments'}
       </h3>
       <div className="divide-y divide-[#8C92B3]/25">
         {comments.map((comment) => (
-          <Comment
+          <BaseComment
             key={comment.id}
             as="comment"
             comment={comment}
             replies={(reply) => (
-              <Comment key={reply.id} as="reply" comment={reply} />
+              <BaseComment key={reply.id} as="reply" comment={reply} />
             )}
           />
         ))}
